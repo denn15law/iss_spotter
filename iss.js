3eb070762d3dc22557a200991c5a47de93a875d6
Dennis Law <denn15_law@live.ca>
2021-11-04 23:50:55 +0000

    add fetchCoordsByIP and use callbacks to implement functionality

--- iss.js
+++ iss.js
@@ -12,10 +12,17 @@ const request = require("request");
 const fetchMyIP = function (callback) {
   // use request to fetch IP address from JSON API
   // https://api.ipify.org?format=json
+  //
 
   request("https://api.ipify.org?format=json", (err, res, body) => {
     if (err) {
       callback(err, null);
+      return;
+    }
+
+    if (res.statusCode !== 200) {
+      const msg = `Status Code ${res.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
       return;
     }
 
@@ -25,6 +32,24 @@ const fetchMyIP = function (callback) {
   });
 };
 
-fetchMyIP(() => {});
+const fetchCoordsByIP = function (ip, callback) {
+  request(
+    `https://api.freegeoip.app/json/invalidip?apikey=03e55630-3dc1-11ec-951e-abdadbf63c9f`,
+    (err, res, body) => {
+      if (err) {
+        callback(err, null);
+        return;
+      }
 
-module.exports = { fetchMyIP };
+      if (res.statusCode !== 200) {
+        const msg = `Status Code ${res.statusCode} when fetching coordinates for IP. Response: ${body}`;
+        callback(msg, null);
+        return;
+      }
+      const { latitude, longitude } = JSON.parse(body);
+      callback(null, { latitude, longitude });
+    }
+  );
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
