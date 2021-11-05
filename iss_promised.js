b6c2a98e4cf66b74217720ba05329cb305b97941
Dennis Law <denn15_law@live.ca>
2021-11-05 03:24:48 +0000

    refractor and add iss_promised and index2 for promise implementation

---
+++ iss_promised.js
@@ -0,0 +1,40 @@
+const request = require("request-promise-native");
+
+const fetchMyIP = function () {
+  return request("https://api.ipify.org?format=json");
+};
+
+const fetchCoordsByIP = function (body) {
+  const ip = JSON.parse(body).ip;
+  return request(
+    `https://api.freegeoip.app/json/${ip}?apikey=03e55630-3dc1-11ec-951e-abdadbf63c9f`
+  );
+};
+
+const fetchISSFlyOverTimes = function (body) {
+  const { latitude, longitude } = JSON.parse(body);
+  return request(
+    `https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`
+  );
+};
+
+const nextISSTimesForMyLocation = function () {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+const printPassTimes = function (passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+module.exports = { nextISSTimesForMyLocation, printPassTimes };
